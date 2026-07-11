d0dccd139df8f6b445ddbc7b5f08b53c77871a91
Madheswaran Selvakumar <madheswaran.selvakumar@gmail.com>
2026-07-11 19:06:08 +0000

    Add paypal frontend project

---
+++ devops/paypal-frontend-app/frontend-service/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template_string
+#from app import app
+
+app = Flask(__name__)
+
+HOME_PAGE = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PayPal Checkout</title>
+</head>
+<body>
+
+<h1>PayPal Checkout</h1>
+
+<h3>Welcome</h3>
+
+<form>
+
+Email
+
+<br>
+
+<input type="text">
+
+<br><br>
+
+Password
+
+<br>
+
+<input type="password">
+
+<br><br>
+
+<button>Login</button>
+
+</form>
+
+</body>
+</html>
+"""
+
+@app.route("/")
+def home():
+    return render_template_string(HOME_PAGE)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3001)
+
+
+#def test_home():
+#    client = app.test_client()
+#    response = client.get("/")
+#    assert response.status_code == 200
