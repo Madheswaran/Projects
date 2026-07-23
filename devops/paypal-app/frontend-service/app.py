a5276707c0a444b2d2652f7bfec224b687fdd226
Madheswaran Selvakumar <madheswaran.selvakumar@gmail.com>
2026-07-23 11:53:30 +0000

    ui page

--- devops/paypal-app/frontend-service/app.py
+++ devops/paypal-app/frontend-service/app.py
@@ -11,58 +11,229 @@ app = Flask(__name__)
 
 #os.system(user_input)
 
-HOME_PAGE = """
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>PayPal Frontend Service</title>
+
+<title>{{ APP_NAME }}</title>
+
+<style>
+
+*{
+margin:0;
+padding:0;
+box-sizing:border-box;
+font-family:Arial,Helvetica,sans-serif;
+}
+
+body{
+
+background:#eef2f7;
+
+display:flex;
+
+justify-content:center;
+
+align-items:center;
+
+height:100vh;
+
+}
+
+.container{
+
+width:1100px;
+
+height:650px;
+
+display:flex;
+
+background:white;
+
+border-radius:18px;
+
+overflow:hidden;
+
+box-shadow:0 15px 40px rgba(0,0,0,.25);
+
+}
+
+.left{
+
+width:40%;
+
+padding:60px;
+
+display:flex;
+
+flex-direction:column;
+
+justify-content:center;
+
+background:white;
+
+}
+
+.left h1{
+
+font-size:34px;
+
+color:#003087;
+
+margin-bottom:15px;
+
+}
+
+.left h3{
+
+margin-bottom:40px;
+
+color:#666;
+
+}
+
+input{
+
+width:100%;
+
+padding:14px;
+
+margin-top:8px;
+
+margin-bottom:25px;
+
+border-radius:8px;
+
+border:1px solid #ccc;
+
+font-size:16px;
+
+}
+
+button{
+
+width:100%;
+
+padding:15px;
+
+background:#0070ba;
+
+color:white;
+
+font-size:18px;
+
+border:none;
+
+border-radius:8px;
+
+cursor:pointer;
+
+transition:.3s;
+
+}
+
+button:hover{
+
+background:#005ea6;
+
+}
+
+.right{
+
+width:60%;
+
+background:#f7f9fc;
+
+display:flex;
+
+justify-content:center;
+
+align-items:center;
+
+padding:40px;
+
+}
+
+.right img{
+
+width:100%;
+
+max-width:650px;
+
+}
+
+.footer{
+
+margin-top:20px;
+
+font-size:13px;
+
+color:gray;
+
+text-align:center;
+
+}
+
+</style>
+
 </head>
 
 <body>
 
+<div class="container">
+
+<div class="left">
+
 <h1>{{ APP_NAME }}</h1>
 
-<h2>Environment : {{ ENV_NAME }}</h2>
+<h3>Environment : {{ ENV_NAME }}</h3>
 
-<h3>Welcome</h3>
-
-<form method="POST" action="/login">
+<form action="/login" method="POST">
 
 <label>Email</label>
 
-<br>
-
 <input
-    type="email"
-    name="email"
-    placeholder="Enter Email"
-    required>
-
-<br><br>
+name="email"
+type="email"
+placeholder="Enter Email"
+required>
 
 <label>Password</label>
 
-<br>
-
 <input
-    type="password"
-    name="password"
-    placeholder="Enter Password"
-    required>
-
-<br><br>
+name="password"
+type="password"
+placeholder="Enter Password"
+required>
 
 <button type="submit">
-    Login
+Login
 </button>
 
 </form>
 
+<div class="footer">
+
+Secure Checkout • Powered by Kubernetes
+
+</div>
+
+</div>
+
+<div class="right">
+
+<img
+src="{{ url_for('static', filename='shopping-bg.png') }}"
+alt="Shopping">
+
+</div>
+
+</div>
+
 </body>
 
 </html>
-"""
 
 @app.route("/login", methods=["POST"])
 def login():
