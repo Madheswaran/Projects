36565f3f2dd31a16cb0fc7e165bd238b41e2c4ef
Madheswaran Selvakumar <madheswaran.selvakumar@gmail.com>
2026-07-23 18:10:39 +0000

    unit test ui

--- devops/paypal-app/frontend-service/tests/test_app.py
+++ devops/paypal-app/frontend-service/tests/test_app.py
@@ -14,12 +14,42 @@ sys.path.append(
 os.environ["API_KEY"] = "test-api-key"
 os.environ["DB_PASSWORD"] = "test-password"
 
-from app import app 
+from app import app
 
-def test_home():
+
+def test_home_redirect():
 
     client = app.test_client()
 
-    response = client.get('/')
+    response = client.get("/")
+
+    assert response.status_code == 302
+
+
+def test_login_page():
+
+    client = app.test_client()
+
+    response = client.get("/login")
+
+    assert response.status_code == 200
+
+    assert b"Login" in response.data
+
+
+def test_health():
+
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+
+
+def test_ready():
+
+    client = app.test_client()
+
+    response = client.get("/ready")
 
     assert response.status_code == 200
