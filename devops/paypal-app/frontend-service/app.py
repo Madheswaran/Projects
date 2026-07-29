62d8042bacb87d3fb8cbadecd9e249e156331377
Madheswaran Selvakumar <madheswaran.selvakumar@gmail.com>
2026-07-29 10:16:51 +0000

    prometheus metrics added

--- devops/paypal-app/frontend-service/app.py
+++ devops/paypal-app/frontend-service/app.py
@@ -1,3 +1,6 @@
+from prometheus_client import generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST
+
 from flask import (
     Flask,
     render_template,
@@ -10,6 +13,7 @@ from flask import (
 import os
 import requests
 import psycopg2
+from flask import Response
 
 app = Flask(__name__)
 app.secret_key = "paypal-secret-key"
@@ -395,7 +399,17 @@ def ready():
         "status": "READY"
     }, 200
 
+#################################################
+# Metrics
+#################################################
 
+@app.route("/metrics")
+def metrics():
+
+    return Response(
+        generate_latest(),
+        mimetype=CONTENT_TYPE_LATEST
+    )
 # ---------------------------------------------------
 # Main
 # ---------------------------------------------------
